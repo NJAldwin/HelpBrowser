e8101e1e3f776438f0ae35f46599012afa4cfff2
Nick Aldwin <NJAldwin@gmail.com>
2011-08-15 00:18:48 -0400

    Add initial version

---
+++ HelpBrowser.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ShakenSW.HelpBrowser
+{
+    partial class HelpBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // uxWebBrowser
+            // 
+            this.uxWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.uxWebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.uxWebBrowser.Margin = new System.Windows.Forms.Padding(0);
+            this.uxWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.uxWebBrowser.Name = "uxWebBrowser";
+            this.uxWebBrowser.Size = new System.Drawing.Size(624, 442);
+            this.uxWebBrowser.TabIndex = 0;
+            this.uxWebBrowser.DocumentTitleChanged += new System.EventHandler(uxWebBrowser_DocumentTitleChanged);
+            // 
+            // HelpBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.uxWebBrowser);
+            this.MinimumSize = new System.Drawing.Size(30, 38);
+            this.Name = "HelpBrowser";
+            this.ShowIcon = false;
+            this.Text = "Help Browser";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser uxWebBrowser;
+    }
+}
---
+++ HelpBrowser.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace ShakenSW.HelpBrowser
+{
+    /// <summary>
+    /// A window that lets the user browse an online help file, using an offline file as backup if internet access is unavailable.
+    /// </summary>
+    /// <remarks>Must be run from a STA thread.</remarks>
+    public partial class HelpBrowser : Form
+    {
+        /// <summary>
+        /// The URI to display if internet access is available
+        /// </summary>
+        public Uri OnlineUri { get; set; }
+        /// <summary>
+        /// The URI to display if internet access is unavailable
+        /// </summary>
+        public Uri OfflineUri { get; set; }
+
+        /// <summary>
+        /// Initializes a new <see cref="HelpBrowser"/> with the specified URIs
+        /// </summary>
+        /// <param name="onlineUri">The URI to display if internet access is available</param>
+        /// <param name="offlineUri">The URI to display if internet access is unavailable</param>
+        public HelpBrowser(Uri onlineUri, Uri offlineUri)
+        {
+            OnlineUri = onlineUri;
+            OfflineUri = offlineUri;
+
+            // Insert menu items to allow user to force online/offline
+            IntPtr menuHandle = GetSystemMenu(this.Handle, false);
+            InsertMenu(menuHandle, 5, MF_BYPOSITION | MF_SEPARATOR, 0, string.Empty);
+            InsertMenu(menuHandle, 6, MF_BYPOSITION, MENU_ONLINE, "Use Online Help");
+            InsertMenu(menuHandle, 7, MF_BYPOSITION, MENU_OFFLINE, "Use Offline Help");
+
+            InitializeComponent();
+
+            uxWebBrowser.Url = OnlineAvailable(OnlineUri)
+                                ? OnlineUri
+                                : OfflineUri;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="HelpBrowser"/> with the specified URIs
+        /// </summary>
+        /// <param name="onlineUri">The URI to display if internet access is available</param>
+        /// <param name="offlineUri">The URI to display if internet access is unavailable</param>
+        public HelpBrowser(string onlineUri, string offlineUri)
+            :this(new Uri(onlineUri), new Uri(offlineUri))
+        {
+        }
+
+        void uxWebBrowser_DocumentTitleChanged(object sender, EventArgs e)
+        {
+            Text = uxWebBrowser.DocumentTitle;
+        }
+
+        /// <summary>
+        /// Returns whether or not the URI is available &amp; returns 200 OK.
+        /// </summary>
+        /// <param name="uri">The URI to test</param>
+        /// <returns><c>true</c> if the URI is available &amp; returns 200 OK; otherwise, <c>false</c></returns>
+        private bool OnlineAvailable(Uri uri)
+        {
+            try
+            {
+                return ((HttpWebResponse)WebRequest.Create(uri).GetResponse()).StatusCode == HttpStatusCode.OK;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // Imports
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        private static extern bool InsertMenu(IntPtr hMenu, Int32 wPosition, Int32 wFlags, Int32 wIDNewItem,
+                                              string lpNewItem);
+
+        // Commands
+        private const Int32 WM_SYSCOMMAND = 0x112;
+        private const Int32 MF_SEPARATOR = 0x800;
+        private const Int32 MF_BYPOSITION = 0x400;
+
+        // Menu Items
+        private const Int32 MENU_ONLINE = 1000;
+        private const Int32 MENU_OFFLINE = 1001;
+
+        /// <summary>
+        /// Handles window messages 
+        /// (here, handles clicks on the menu items)
+        /// </summary>
+        /// <param name="msg">The message to handle</param>
+        protected override void WndProc(ref Message msg)
+        {
+            if (msg.Msg == WM_SYSCOMMAND)
+            {
+                switch (msg.WParam.ToInt32())
+                {
+                    case MENU_ONLINE:
+                        uxWebBrowser.Url = OnlineUri;
+                        return;
+                    case MENU_OFFLINE:
+                        uxWebBrowser.Url = OfflineUri;
+                        return;
+                    default:
+                        break;
+                }
+            }
+            base.WndProc(ref msg);
+        }
+    }
+}
